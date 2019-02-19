efabcbf8c868db89a5dedc1515266ecd06762087
Martin Ševčík <martin.sevcik.95@gmail.com>
2019-02-19 17:13:47 +0100

    Initial Commit

---
+++ HustleUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HustleUp
+//
+//  Created by Martin Ševčík on 19/02/2019.
+//  Copyright © 2019 Martin Ševčík. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
