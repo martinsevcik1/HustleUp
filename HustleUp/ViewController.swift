b5f60f73cc0eeeeede29a0a2b5cca25e5fc4f376
Martin Ševčík <martin.sevcik.95@gmail.com>
2019-02-19 19:28:28 +0100

    commit

--- HustleUp/ViewController.swift
+++ HustleUp/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var Button: UIButton!
+    @IBOutlet weak var CloudHolder: UIView!
+    @IBOutlet weak var Rocket: UIImageView!
+    @IBOutlet weak var cloudsAndOrangeOval: UIImageView!
+    @IBOutlet weak var Lbl: UILabel!
+    @IBOutlet weak var On: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
-
-
+    
+    
+    @IBAction func buttonClick(_ sender: Any) {
+    CloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        Button.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.Rocket.frame = CGRect(x: 0, y: 30, width: 375, height: 667)
+        }) { (finished) in
+            self.Lbl.isHidden = false
+            self.On.isHidden = false
+        }
+}
 }
 
